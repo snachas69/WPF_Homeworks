b0b79cf7d8a823c6a2d6c51829c94f9feb5b7c7c
Alex <volexandr2005@gmail.com>
2022-09-25 13:05:23 +0300

    Added the new task

--- _19_09_2022/MainWindow.xaml.cs
+++ _19_09_2022/MainWindow.xaml.cs
@@ -1,29 +1,120 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace _19_09_2022
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private Button[,] buttons = new Button[4, 4];
+        private Point emptinessCordinates = new() { X = 3, Y = 3 };
+        private Random random = new();
         public MainWindow()
         {
             InitializeComponent();
+            SetGrid();
+            ShuffleGrid();
+        }
+
+        private void SetGrid()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (buttons.GetLength(0) * i + j == 15)
+                        break;
+                    
+                    buttons[i, j] = new Button()
+                    {
+                        Content = (buttons.GetLength(0) * i + j) + 1,
+                        Style = (Style)Resources["NumericButton"]
+                    };
+                    field.Children.Add(buttons[i, j]);
+                    Grid.SetColumn(buttons[i, j], j);
+                    Grid.SetRow(buttons[i, j], i);
+                }
+            }
             
+            buttons[3, 3] = new Button() { Style = (Style)Resources["TotalTransperecy"] };
+            field.Children.Add(buttons[3,3]);
+            Grid.SetColumn(buttons[3,3], 3);
+            Grid.SetRow(buttons[3,3], 3);
+        }
+
+        private void ShuffleGrid()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                int x = 0, y = 0;
+                
+                do
+                {
+                    x = random.Next(100) % 3; if (x == 2) x = -1;
+                    y = random.Next(100) % 3; if (y == 2) y = -1; 
+                } while ((int)emptinessCordinates.X + x > 3 || (int)emptinessCordinates.X + x < 0
+                       || (int)emptinessCordinates.Y + y > 3 || (int)emptinessCordinates.Y + y < 0);
+                
+                SwapWithEmptinessIfPossiable(buttons[(int)emptinessCordinates.X + x, (int)emptinessCordinates.Y + y]);
+            }
+        }
+
+        private void SwapWithEmptinessIfPossiable(Button button)
+        {
+            int x = Grid.GetColumn(button);
+            int y = Grid.GetRow(button);
+
+            if (Math.Abs(x - emptinessCordinates.X) == 1 && y == emptinessCordinates.Y
+                || Math.Abs(y - emptinessCordinates.Y) == 1 && x == emptinessCordinates.X)
+            {
+                Button temp = buttons[x, y];
+                buttons[x, y] = buttons[(int)emptinessCordinates.X, (int)emptinessCordinates.Y];
+                buttons[(int)emptinessCordinates.X, (int)(emptinessCordinates.Y)] = temp;
+
+                Grid.SetColumn(button, (int)emptinessCordinates.X);
+                Grid.SetRow(button, (int)emptinessCordinates.Y);
+
+                Grid.SetColumn(buttons[x, y], x);
+                Grid.SetRow(buttons[x, y], y);
+
+                emptinessCordinates.X = x;
+                emptinessCordinates.Y = y;
+            }
+        }
+
+        private void Move(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                SwapWithEmptinessIfPossiable(button);
+                if (IsAssabled())
+                {
+                    MessageBox.Show("Puzzle is assambled!");
+                    Environment.Exit(0);
+                }
+
+            }
+        }
+        private bool IsAssabled()
+        {
+            for (int i = 0; i < buttons.GetLength(0); i++)
+                for (int j = 0; j < buttons.GetLength(1); j++)
+                {
+                    if (buttons[i, j].Content is null)
+                        continue;
+                    if (buttons[i, j].Content.ToString() != ((buttons.GetLength(0) * i + j) + 1).ToString())
+                        return false;
+                }
+          
+            return true;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < buttons.GetLength(0); i++)
+                for (int j = 0; j < buttons.GetLength(1); j++)
+                    if (buttons[i, j].Content is not null)
+                        buttons[i, j].Click += Move;
         }
     }
 }
